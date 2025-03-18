7d53a5b4693e6e06a8cdaa8483da1393b07c9d78
akroZora <schreyerjulian@googlemail.com>
2025-03-18 17:35:22 +0100

    fixed errors

--- shl/array_sort.h
+++ shl/array_sort.h
@@ -22,7 +22,7 @@ namespace shl {
 	inline void reverseArray(T* arr, size_t size) {
 		size_t half = size / 2;
 		for (size_t i = 0; i < half; ++i) {
-			std::swap(&arr[i], &arr[size - i]);
+			std::swap(arr[i], arr[size - i]);
 		}
 	}
 	template<typename T>
@@ -33,14 +33,12 @@ namespace shl {
 	template<typename T>
 	inline void insertionSort(T* toSort, size_t size) {
 		for (size_t i = 1; i < size; ++i) {
-			size_t key = toSort[i];
-			size_t j = i - 1;
-
-			while (j >= 0 && toSort[j] > key) {
-				toSort[j + 1] = toSort[j];
-				j = j - 1;
+			T key = toSort[i];
+			size_t j = i;
+			for (; j > 0 && toSort[j-1] > key; --j) {
+				toSort[j] = toSort[j-1];
 			}
-			toSort[j + 1] = key;
+			toSort[j] = key;
 		}
 	}
 	template<typename T>
@@ -51,60 +49,63 @@ namespace shl {
 	template<typename T>
 	inline size_t pickPivot(T* toSort, size_t start, size_t end) {
 		assert(start < end);
-		size_t mid = (end - start) / 2;
-		size_t lower = toSort[start] < toSort[end] ? start : end;
-		return toSort[mid] < toSort[lower] ? lower : mid;
+		size_t mid = start + (end - start) / 2;
+		if (toSort[start] < toSort[end]) {
+			if (toSort[end] < toSort[mid]) return end;
+			else if (toSort[start] < toSort[mid]) return mid;
+			else return start;
+		}
+		else {
+			if (toSort[start] < toSort[mid]) return start;
+			else if (toSort[end] < toSort[mid]) return mid;
+			else return end;
+		}
 	}
 	template<typename T>
 	inline size_t swapAroundPivot(T* toSort, size_t start, size_t end, size_t pivot) {
 		assert(start < end);
 		assert(pivot <= end);
 		assert(start <= pivot);
-		constexpr T& value = toSort[pivot];
-		std::swap(&toSort[pivot], &toSort[end]);
-		size_t lower = end - 1;
-		size_t higher = start;
+		std::swap(toSort[pivot], toSort[end]);
+		size_t smaller = end - 1;
+		size_t greater = start;
 		while (true) {
-			for (; start < lower; --lower) {
-				if (toSort[lower] < value)
-					break;
+			while (start < smaller && toSort[smaller] > toSort[end]) {
+				--smaller;
 			}
-			for (; higher < end; ++higher) {
-				if (value < toSort[higher])
-					break;
+			while (greater < end && toSort[end] >= toSort[greater]) {
+				++greater;
 			}
-			if (lower < higher) {
+			if (greater >= smaller)
 				break;
-			}
-			std::swap(&toSort[higher], &toSort[lower]);
+			std::swap(toSort[greater], toSort[smaller]);
 		}
-		std::swap(&toSort[higher], &toSort[end]);
-		return higher;
+		std::swap(toSort[greater], toSort[end]);
+		return greater;
 	}
 	template<typename T>
 	inline void quickSortSlice(T* toSort, size_t start, size_t end) {
-		size_t pivot = pickPivot(toSort, start, end);
-		pivot = swapAroundPivot(toSort, start, end, pivot);
-		if (pivot < end) {
+		if (start < end) {
+			size_t pivot = pickPivot(toSort, start, end);
+			pivot = swapAroundPivot(toSort, start, end, pivot);
 			quickSortSlice(toSort, start, pivot - 1);
 			quickSortSlice(toSort, pivot + 1, end);
 		}
 	}
 	template<typename T>
 	inline void quickSort(T* toSort, size_t size) {
-		quickSortSlice(toSort, 0, size);
+		quickSortSlice(toSort, 0, size - 1);
 	}
 	template<typename T>
 	inline void quickSort(std::vector<T>& toSort) {
-		quickSortSlice(toSort.data(), 0, toSort.size());
+		quickSortSlice(toSort.data(), 0, toSort.size() - 1);
 	}
 
 	template<typename T>
 	inline void bubbleSort(T* toSort, size_t size) {
-		for (size_t i = 0; i < size - 1; ++i) {
-
+		for (size_t i = 1; i < size; ++i) {
 			bool swapped = false;
-			for (size_t j = 0; j < size - i - 1; ++j) {
+			for (size_t j = 0; j < size - i; ++j) {
 				if (toSort[j] > toSort[j+1]) {
 					std::swap(toSort[j], toSort[j + 1]);
 					swapped = true;
@@ -115,7 +116,7 @@ namespace shl {
 		}
 	}
 	template<typename T>
-	inline void bubbleSort(std::vector<T> toSort) {
+	inline void bubbleSort(std::vector<T>& toSort) {
 		bubbleSort(toSort.data(), toSort.size());
 	}
 	
