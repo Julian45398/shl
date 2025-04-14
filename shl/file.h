239dab7b51c6a4b160a300cf24f71e1c2c2cc245
Julian Schreyer <schreyerjulian@googlemail.com>
2025-04-14 17:08:31 +0200

    fixed missing return in readBinaryFile and added readRequiredBinaryFile

--- shl/file.h
+++ shl/file.h
@@ -5,10 +5,25 @@
 #include <vector>
 
 namespace shl {
+	inline std::vector<char> readRequiredBinaryFile(const char* filename) {
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+		if (!file.is_open()) {
+			shl::logFatal("Failed to open file: ", filename);
+		}
+
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+		file.close();
+		return buffer;
+	}
 	inline std::vector<char> readBinaryFile(const char* filename) {
 		std::ifstream file(filename, std::ios::ate | std::ios::binary);
 		if (!file.is_open()) {
-			shl::logError("Failed to open file: ", filename);
+			shl::logWarn("Failed to open file: ", filename);
+			return std::vector<char>();
 		}
 
 		size_t fileSize = (size_t)file.tellg();
